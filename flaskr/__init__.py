da6455851406bf21efce1ef1a5bb542a1e18e329
fengpengjun <fpengjun@gamil.com>
2020-08-13 08:22:14 +0800

    修复蓝图not found 和完成登录注册功能

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -23,5 +23,5 @@ def create_app(test_config=None):
 
     from . import db,auth
     db.init_app(app)
-    auth.register_blueprint(auth.bp)
+    app.register_blueprint(auth.bp)
     return app
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -6,9 +6,9 @@ from flask import(
 
 from werkzeug.security import check_password_hash,generate_password_hash
 
-from flask.db import get_db
+from flaskr.db import get_db
 
-bp = Buleprint('auth',__name__,url_prefix='/auth')
+bp = Blueprint('auth',__name__,url_prefix='/auth')
 
 @bp.route('/register',methods = ['GET','POST'])
 def register():
@@ -23,14 +23,14 @@ def register():
         elif not password:
             error = 'password is required'
         elif db.execute(
-            'SELECT id  FROM user WHERE username ? ',(username,)
+            'SELECT id  FROM user WHERE username= ? ',(username,)
         ).fetchone() is not None:
             error = 'user {} is already registered'.format(username)
 
         if error is None:
             db.execute(
-                'INSERT INTO user(username,generate_password_hash(password)) values (?,?)',
-                (username,password)
+                'INSERT INTO user(username,password) values (?,?)',
+                (username,generate_password_hash(password))
             )
             db.commit()
             return redirect(url_for('auth.login'))
@@ -41,7 +41,7 @@ def register():
 @bp.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('auth/login.html')
     elif request.method =='POST':
         username = request.form['username']
         password = request.form['password']
@@ -65,7 +65,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            redirect(url_for('index'))
+            return redirect(url_for('index'))
 
         flash(error)
 
